2c98a394605bb838ec1beb9f60313ffdfac8d42e
Ketan Patil <ketan@retailwhizz.com>
2018-02-02 12:36:19 +0530

    changing the way average is being calculated

--- server/test/simple-for-loop.js
+++ server/test/simple-for-loop.js
@@ -2,6 +2,7 @@
 
 var loopCount = 100;
 var sampleSize = 100;
+var resultSet = [];
 
 if(process.argv.length > 0 && process.argv[2]){
   loopCount = process.argv[2];
@@ -13,17 +14,17 @@ if(process.argv.length > 1 && process.argv[3]){
 console.log("loopCount "+ loopCount);
 console.log("sampleSize "+ sampleSize);
 
-var start = process.hrtime();
 
 for(var i = 0; i<=sampleSize; i++){ // this loop is for averaging results out
+  let start = process.hrtime();
   for(var j = 0; j<=loopCount; j++){ //this loop is for actual iteration
   //do nothing here
   }
+  let end = process.hrtime(start);
+  resultSet.push((((end[0] * 1000 * 1000 * 1000) + end[1])/(1000 * 1000)));
 }
 
-var end = process.hrtime(start);
-console.log(`Average time for ${loopCount} loops with sample size of ${sampleSize} in millis is ` + (((end[0] * 1000 * 1000 * 1000) + end[1])/(1000 * 1000 * sampleSize)));
-
+console.log(`Average time for ${loopCount} loops with sample size of ${sampleSize} in millis is ` + average(resultSet));
 exit();
 
 function exitWithError(err){
@@ -34,4 +35,8 @@ function exitWithError(err){
 function exit(){
   console.log('Exiting the script now!');
   process.exit(0);
+}
+
+function average(arr){
+  return arr.reduce((e1, e2) => e1 + e2,0)/arr.length;
 }
